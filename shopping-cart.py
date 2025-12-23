f089ef180ae66506da8659eaced14482574ad6fe
Kirthika2503 <mskirthika@gmail.com>
2025-12-23 12:20:05 +0530

    added shopping cart script

---
+++ shopping-cart.py
@@ -0,0 +1,26 @@
+# A simple shooping cart program
+
+foods = []
+prices = []
+total = 0
+
+while True:
+  food = input("What food do you like to buy (q to quit): ")
+  
+  if food.lower() == "q": 
+    break
+  else:
+    price = float(input(f"Enter the price of the {food}: $ "))
+    foods.append(food)
+    prices.append(price)
+
+print("YOUR LIST")
+
+for food in foods:
+  print(food, end=" ")
+
+for price in prices:
+  total += price
+
+print()
+print(f"Your total is ${total} ")
